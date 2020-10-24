158e1c448e8ca43b93089141b63d396d5507aae0
jkivlen <jimmykivlen@gmail.com>
2020-10-24 12:53:06 -0500

    removed comments

--- lib/api.rb
+++ lib/api.rb
@@ -7,17 +7,7 @@ class API
         response = HTTParty.get(url)
         parsed = response.parsed_response
         
-
-        abilities_list = parsed["abilities"]
         game_indices_list = parsed["game_indices"]
-    
-        abilities_list.each do |ditto_hash|
-          ditto1 = Ditto.new
-          ditto1.ability_name = ditto_hash["ability"]["name"]
-          ditto1.ability_url = ditto_hash["ability"]["url"]
-          ditto1.ability_is_hidden = ditto_hash["is_hidden"]
-          ditto1.ability_slot = ditto_hash["slot"]
-        end
         
         game_indices_list.each do |ditto_hash|
           ditto1 = Ditto.new            
--- lib/cli.rb
+++ lib/cli.rb
@@ -7,72 +7,24 @@ class CLI
     end
 
     def menu
-        #give user the option to see ditto attributes
-        puts "Would you like to see the abilities or game indeces of Ditto?"
-        puts "Type 'abilities' for abilities, 'indeces' for indeces any other key to exit"
+        puts "Would you like to see Ditto's game indeces?"
+        puts "Type 'yes' to continue or any other key to exit"    
 
         user_input = gets.strip.downcase
-        #if the user says yes    
-        if user_input == "abilities"
-            puts "Good Choice!"
-            #display the list    
-            display_ditto_abilities
-            ask_user_for_ability_choice
 
-            sleep(2)
-            menu
-        elsif user_input == "indeces"
-            puts "Awesome Choice!"
+        if user_input == "yes"
+            puts "Good choice!"
 
             display_ditto_game_indices
             ask_user_for_game_choice
-            
-            #recursion
-            menu 
+
+            sleep(2)
+
+            menu
+
         else
             puts "Goodbye"
         end
-    end
-
-    def display_ditto_abilities
-        # access all the abilities
-        #print each out out
-        
-        Ditto.all.each.with_index(1) do |ditto, index|
-            puts "#{index}. #{ditto.ability_name}"
-        end
-    end
-
-
-    def ask_user_for_ability_choice
-        #ask user for choice
-        puts "Enter the ability number you'd like to know more about"
-        user_input = gets.strip.to_i - 1
-
-        #index valid? number between 0 and 1
-
-        until user_input.between?(0, Ditto.all.length - 1)
-            #keep asking for user input
-            puts "Sorry invalid input.  Choose a valid number"
-            user_input = gets.strip.to_i - 1
-
-        end
-
-
-        ditto_instance = Ditto.all[user_input]
-       
-        display_ability_details(ditto_instance)
-    end
-
-
-    def display_ability_details(ditto)
-        sleep(1)
-
-        puts ditto.ability_name
-        puts "Ability Hidden: #{ditto.ability_is_hidden}"
-        puts "URL: #{ditto.ability_url}"
-        puts "Slot: #{ditto.ability_slot}"
-
     end
 
 
